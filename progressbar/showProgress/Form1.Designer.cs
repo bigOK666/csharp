ef9b73056a7726975ab55e6310c59d46bf3f862a
Lu <zeren.lu@gmail.com>
2021-06-20 21:37:58 +0200

    progress bar and check if Disk exists

---
+++ progressbar/showProgress/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace showProgress
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.startbutton = new System.Windows.Forms.Button();
+            this.status = new System.Windows.Forms.Label();
+            this.cancelbutton = new System.Windows.Forms.Button();
+            this.otherThread = new System.Windows.Forms.Button();
+            this.bw = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(254, 210);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(235, 30);
+            this.progressBar1.TabIndex = 0;
+            this.progressBar1.Visible = false;
+            // 
+            // startbutton
+            // 
+            this.startbutton.Location = new System.Drawing.Point(254, 246);
+            this.startbutton.Name = "startbutton";
+            this.startbutton.Size = new System.Drawing.Size(75, 23);
+            this.startbutton.TabIndex = 1;
+            this.startbutton.Text = "start";
+            this.startbutton.UseVisualStyleBackColor = true;
+            this.startbutton.Click += new System.EventHandler(this.startbutton_Click);
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.Location = new System.Drawing.Point(251, 194);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(35, 13);
+            this.status.TabIndex = 2;
+            this.status.Text = "status";
+            // 
+            // cancelbutton
+            // 
+            this.cancelbutton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelbutton.Location = new System.Drawing.Point(394, 246);
+            this.cancelbutton.Name = "cancelbutton";
+            this.cancelbutton.Size = new System.Drawing.Size(75, 23);
+            this.cancelbutton.TabIndex = 3;
+            this.cancelbutton.Text = "cancel";
+            this.cancelbutton.UseVisualStyleBackColor = true;
+            this.cancelbutton.Click += new System.EventHandler(this.cancelbutton_Click);
+            // 
+            // otherThread
+            // 
+            this.otherThread.Location = new System.Drawing.Point(338, 325);
+            this.otherThread.Name = "otherThread";
+            this.otherThread.Size = new System.Drawing.Size(75, 23);
+            this.otherThread.TabIndex = 4;
+            this.otherThread.Text = "otherThread";
+            this.otherThread.UseVisualStyleBackColor = true;
+            this.otherThread.Click += new System.EventHandler(this.otherThread_Click);
+            // 
+            // bw
+            // 
+            this.bw.WorkerReportsProgress = true;
+            this.bw.WorkerSupportsCancellation = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.otherThread);
+            this.Controls.Add(this.cancelbutton);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.startbutton);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button startbutton;
+        private System.Windows.Forms.Label status;
+        private System.Windows.Forms.Button cancelbutton;
+        private System.Windows.Forms.Button otherThread;
+        private System.ComponentModel.BackgroundWorker bw;
+    }
+}
+
---
+++ progressbar/showProgress/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace showProgress
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            progressBar1.Visible = false;
+            progressBar1.MarqueeAnimationSpeed = 10;
+            progressBar1.Style = ProgressBarStyle.Marquee;
+            status.Visible = false;
+        }
+
+        private async void startbutton_Click(object sender, EventArgs e)
+        {
+            progressBar1.Visible = true;
+            status.Text = "Updating";
+            status.Visible = true;
+            await Task.Delay(2000);
+            status.Text = "Loging in";
+            await Task.Delay(2000);
+            status.Text = "Selftest";
+            await Task.Delay(2000);
+
+            status.Text = "Updated";
+
+        }
+
+        private void cancelbutton_Click(object sender, EventArgs e)
+        {
+            progressBar1.Visible = false;
+            status.Visible = false;
+            status.Text = "canceled";
+            
+        }
+
+        private void otherThread_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Another Thread!", "other thread", MessageBoxButtons.OK);
+            string diskName = "I:\\";
+            uint counterNoDiskWarning = 3;
+            while (!Directory.Exists(diskName) & counterNoDiskWarning!= 0)
+            {
+                MessageBox.Show(string.Format("Please check if {0}", diskName), "Disk is not found", MessageBoxButtons.OK);
+                counterNoDiskWarning--;
+            }
+
+            status.Text = "Other Thread";
+            
+        }
+    }
+}
